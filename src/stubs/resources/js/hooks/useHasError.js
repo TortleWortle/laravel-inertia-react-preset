03b9963f8a617e51ce62fed164bf68fe06a5a82f
TortleWortle <bas.kuipers@me.com>
2020-02-21 20:16:19 +0100

    Removed unused bad debugging line

--- src/stubs/resources/js/hooks/useHasError.js
+++ src/stubs/resources/js/hooks/useHasError.js
@@ -1,6 +1,5 @@
 import { useMemo } from 'react'
 export function useHasError(errors, field) {
-  // return true;
   return useMemo(() => {
     return errors[field] !== undefined
   }, [errors, field]);
